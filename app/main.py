a2b5e121466acd46a8ce313579ffb7f970758fba
Dawid <kucharzjaw@gmail.com>
2025-07-05 10:25:21 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
 class Person:
-    # write your code here
-    pass
+    #"""A class to represent a person with a name and age.""""""
+    #Create a list of Person objects from a list of tuples.
+
+    people = {}
+
+    def __init__ (self, name: str, age: int) -> None:
+        self.name = name
+        self.age= age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    #"""Create a list of Person objects from a list of dictionaries."""
+    person_list = []
+    for person_dict in people:
+        person = Person(person_dict["name"], person_dict["age"])
+        person_list.append(person)
+
+    for person_dict in people:
+        if "wife" in person_dict and person_dict["wife"] is not None:
+            person = Person.people[person_dict["name"]]
+            person.wife = Person.people[person_dict["wife"]]
+        if "husband" in person_dict and person_dict["husband"] is not None:
+            person = Person.people[person_dict["name"]]
+            person.husband = Person.people[person_dict["husband"]]
+    return person_list
+
+
+
